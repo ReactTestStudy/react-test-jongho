141b0fe346282d931c3e7a3c244eb6820e158bfe
ljh95 <ilegwmam@gmail.com>
2021-11-07 15:09:31 +0900

    첫 번째 TDD: counter == 0?

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이ㅛㅇ해서원하는 엘리먼트에 접근(접근할 떄 Id로)
+  const counterElement = screen.getByTestId('counter');
 
-  const lintTest = screen.getByRole('button', {
-    name: 'lintTest',
-  });
-
-  expect(lintTest.textContent).toBe('lintTest');
+  // id가 counter인 엘레멘트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent('0');
 });
