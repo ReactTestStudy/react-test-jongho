92f6abd661e334078b9f38f75dc89d163e94f58e
ljh95 <ilegwmam@gmail.com>
2021-11-07 17:43:21 +0900

    Delete, Unessential files

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [disabled, setDisabled] = useState(false);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3 data-testid="counter">{counter}</h3>
-
-        <div>
-          <button
-            data-testid="minus-button"
-            onClick={() => setCounter((counter) => counter - 1)}
-            disabled={disabled}
-          >
-            -
-          </button>
-          <button
-            data-testid="plus-button"
-            onClick={() => setCounter((counter) => counter + 1)}
-            disabled={disabled}
-          >
-            +
-          </button>
-        </div>
-
-        <button
-          data-testid="on/off-button"
-          style={{ backgroundColor: 'blue' }}
-          onClick={() => setDisabled((disabled) => !disabled)}
-        >
-          on/off
-        </button>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
