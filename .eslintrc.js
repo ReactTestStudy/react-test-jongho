8637287b89f9240eb238a4707e450dc161d37fed
ljh95 <ilegwmam@gmail.com>
2021-11-07 14:38:05 +0900

    Set eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['import', 'prettier'],
+  rules: {},
+};
