d83bfb3ae3de69748612d9024e6a7a9c3fd5a151
ljh95 <ilegwmam@gmail.com>
2021-11-08 21:57:16 +0900

    FixBug, 무한 렌더링

--- src/pages/OrderPage/Type.tsx
+++ src/pages/OrderPage/Type.tsx
@@ -37,7 +37,7 @@ const Type = ({ orderType }: Props) => {
     return () => {
       isMounted = false;
     };
-  }, [loadItems]);
+  }, []);
 
   const ItemComponents = orderType === 'products' ? Products : Options;
 
