cc20e1eade70ea1a5136411a72a0033b17a5cd8e
ljh95 <ilegwmam@gmail.com>
2021-11-13 15:31:52 +0900

    Add, Calculate test case to check Total Cost

--- README.md
+++ README.md
@@ -116,3 +116,12 @@ UserEvent 사용이 더 좋다 왜?
 # lint가 잘 안먹히는 오류 있는듯
 
 await, async나 더 정확한 matcher를 추천해주지 않음
+
+## userEvent.clear()
+
+input 이나 textarea에 텍스트를 선택(select)한 후 제거(delete)해 줍니다.
+
+이 부분은 현재는 없어도 테스트 결과에 영항을 미치지는 않습니다.
+하지만 만약 현재 소스 코드 보다 위에서 같은 엘리먼트를 위한
+userEvent를 사용했다면, clear 해준 후에
+userEvent.type()을 사용하는게 좋습니다.
---
+++ src/pages/OrderPage/tests/calculate.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Type from '../Type';
+
+it("update product's total when products change", async () => {
+  render(<Type orderType="products" />);
+
+  const productsTotal = screen.getByText('상품 총 가격:', { exact: false }); // 뒤에 문자가 더 있어서 가져올 수 있게 한다
+  expect(productsTotal).toHaveTextContent('0');
+
+  // 아메리카 여행 상품 한개 올리기
+  const americaInput = await screen.findByRole('spinbutton', {
+    // getByRole대신 쓴 이유는 이 부분도 서버가 여행 상품에 대한 정보를 가져온 다음에, 값을 가질 수 있기 때문에, findByRole 한다.
+    name: 'America',
+  });
+
+  userEvent.clear(americaInput);
+  userEvent.type(americaInput, '1');
+  expect(productsTotal).toHaveTextContent('1000');
+  /**
+   * 아메리카가 하나 들어갔기 때문에
+   * 총 가격이 1000원이 되었는지 확인해보는 테스트케이스 입니다.
+   */
+});
